e44ba063329d958e1a550fa906f1ca0df1a6670b
chenzb <1254806967@qq.com>
2025-07-11 10:04:12 +0800

    Initial commit

---
+++ src/main/java/com/xiaotian/ae/wirelesscable/AEWirelessCable.java
@@ -0,0 +1,17 @@
+package com.xiaotian.ae.wirelesscable;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = AEWirelessCable.modID, name = AEWirelessCable.modName, version = AEWirelessCable.version,
+        dependencies = "required-after:forge@[14.21.0.2371,);" +
+                "after:appliedenergistics2@[rv6-stable-7,);",
+        acceptedMinecraftVersions = "[1.12, 1.13)",)
+public class AEWirelessCable {
+
+    public static final String modID = "aewirelesscable";
+    public static final String modName = "AE: Wireless Cable";
+    public static final String version = Tags.VERSION;
+
+
+
+}
